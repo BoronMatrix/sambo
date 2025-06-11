201e5e2a9bbb31ca4dc84c71fcd9a34464b48d76
anita <anitachiu1104@126.com>
2025-06-11 17:16:21 +0800

    web

--- micro_sam/instance_segmentation.py
+++ micro_sam/instance_segmentation.py
@@ -798,10 +798,12 @@ def get_unetr(
 
                 else:  # Whether be strict on finding the parameter in the decoder state.
                     if k not in decoder_state:
-                        raise RuntimeError(f"The parameters for '{k}' could not be found.")
-                    unetr_state_dict[k] = decoder_state[k]
+                        # raise RuntimeError(f"The parameters for '{k}' could not be found.")
+                        pass;
+                    else:
+                        unetr_state_dict[k] = decoder_state[k]
 
-        unetr.load_state_dict(unetr_state_dict)
+        unetr.load_state_dict(unetr_state_dict, strict=False)
 
     unetr.to(device)
     return unetr
